2fd4c70ebde8dd6a6b88c59fd8af60a2af4f8956
sheila <sheilaaachan@gmail.com>
2013-09-09 20:15:32 -0400

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schanapp::Application.config.secret_token = '74bc386233b819286e6ca19dbb92c1f1d7d0aaf091778e4f42c10fcd2908679b92076b3ff531b63ef9b104064902dd91fdb8edd0e5ea91aeb3ba943a73512d81'
